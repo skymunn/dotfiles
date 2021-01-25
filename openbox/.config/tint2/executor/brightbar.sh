22a31666e2d41992c3f946538139cd8421c93f8c
Ikramullah <ikramullahzx@gmail.com>
2021-01-25 22:03:16 +0800

    Modified tint2rc executor and make some change

--- openbox/.config/tint2/executor/brightbar.sh
+++ openbox/.config/tint2/executor/brightbar.sh
@@ -1,47 +1,47 @@
 #!/bin/bash
 
-bright="$(brightnessctl g)"
+bright="$(bash -c ~/.config/tint2/executor/brightness.sh)"
+bright="$(printf '%.*f\n' 0 $bright)"
 
-if [[ "$bright" -ge 230 && "$bright" -le 478 ]];
+if [[ "$bright" -ge 1 && "$bright" -le 10 ]];
 	then
 		echo "━┉┉┉┉┉┉┉┉┉"
 
-elif [[ "$bright" -ge 479 && "$bright" -le 975 ]];
+elif [[ "$bright" -ge 11 && "$bright" -le 20 ]];
 	then
 		echo "━━┉┉┉┉┉┉┉┉"
 
-elif [[ "$bright" -ge 976 && "$bright" -le 1454 ]];
+elif [[ "$bright" -ge 21 && "$bright" -le 30 ]];
 	then
 		echo "━━━┉┉┉┉┉┉┉"
 
-elif [[ "$bright" -ge 1455 && "$bright" -le 1952 ]];
+elif [[ "$bright" -ge 31 && "$bright" -le 40 ]];
 	then
 		echo "━━━━┉┉┉┉┉┉"
 
-elif [[ "$bright" -ge 1953 && "$bright" -le 2430 ]];
+elif [[ "$bright" -ge 41 && "$bright" -le 50 ]];
 	then
 		echo "━━━━━┉┉┉┉┉"
 
-elif [[ "$bright" -ge 2431 && "$bright" -le 2927 ]];
+elif [[ "$bright" -ge 51 && "$bright" -le 60 ]];
 	then
 		echo "━━━━━━┉┉┉┉"
 
-elif [[ "$bright" -ge 2928 && "$bright" -le 3407 ]];
+elif [[ "$bright" -ge 61 && "$bright" -le 70 ]];
 	then
 		echo "━━━━━━━┉┉┉"
 
-elif [[ "$bright" -ge 3408 && "$bright" -le 3905 ]];
+elif [[ "$bright" -ge 71 && "$bright" -le 80 ]];
 	then
 		echo "━━━━━━━━┉┉"
 
-elif [[ "$bright" -ge 3906 && "$bright" -le 4384 ]];
+elif [[ "$bright" -ge 81 && "$bright" -le 90 ]];
 	then
 		echo "━━━━━━━━━┉"
 
-elif [[ "$bright" -eq 4882 ]];
+elif [[ "$bright" -ge 91 && "$bright" -le 100 ]];
 	then
 		echo "━━━━━━━━━━"
 else
  echo "error, brightnessctl not found!"
-
 fi
