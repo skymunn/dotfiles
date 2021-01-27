fe5175a2f61428e25e58ba333892926fd3f60ed9
Ikramullah <ikramullahzx@gmail.com>
2021-01-27 15:13:21 +0800

    Add killing animation in xmenu

--- openbox/.config/xmenu/launch.sh
+++ openbox/.config/xmenu/launch.sh
@@ -16,6 +16,9 @@ Openbox
 	Configure XMenu		konsole -e nano ~/.config/xmenu/launch.sh
 	Configure Tint2		tint2conf
 	Configure urxvt		konsole -e nano ~/.Xresources
+Animation
+	Turn On			picom &
+	Turn Off		killall picom
 
 Power Menu			~/.config/rofi/powermenu/powermenu.sh
 EOF
